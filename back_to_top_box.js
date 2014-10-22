4cdc5679c53f0590ea3cc990d59296f48f7fb7b4
Norman Leymann <norman.leymann@gmail.com>
2014-10-22 17:17:17 +0200

    added script

---
+++ back_to_top_box.js
@@ -0,0 +1,26 @@
+(function ($) {
+  Drupal.behaviors.BackToTop = {
+    attach: function (context, settings) {
+
+      $('.view-portfolio-page').append('<a href="#" class="back-to-top">Back to Top</a>');
+
+      var offset = 220;
+      var duration = 500;
+      jQuery(window).scroll(function() {
+          if (jQuery(this).scrollTop() > offset) {
+              jQuery('.back-to-top').fadeIn(duration);
+          } else {
+              jQuery('.back-to-top').fadeOut(duration);
+          }
+      });
+      
+      jQuery('.back-to-top').click(function(event) {
+          event.preventDefault();
+          jQuery('html, body').animate({scrollTop: 0}, duration);
+          return false;
+      })
+
+      // http://www.developerdrive.com/2013/07/using-jquery-to-add-a-dynamic-back-to-top-floating-button-with-smooth-scroll/
+    }
+  };
+}) (jQuery);
